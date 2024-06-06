8f7295c74f2a412689bb583113d851bb4a753f43
Ludmila Pashkevych <161248085+iclownic@users.noreply.github.com>
2024-06-06 19:43:08 +0300

    Add files via upload

---
+++ To-do List API (w!o calendar)/WebApplication1/Database.cs
@@ -0,0 +1,35 @@
+﻿using Npgsql;
+using ToDoApi.Model;
+using Microsoft.Extensions.Configuration;
+
+namespace WebApplication1
+{
+    public class Database
+    {
+        private readonly string _connectionString;
+
+        public Database(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task InsertToDoAsync(ToDo toDo)
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection(_connectionString))
+            {
+                string sql = "INSERT INTO public.\"TooDoo\"(\"Title\", \"Description\", \"Status\", \"Date\") VALUES (@Title, @Description, @Status, @Date)";
+                await con.OpenAsync();
+
+                using (NpgsqlCommand comm = new NpgsqlCommand(sql, con))
+                {
+                    comm.Parameters.AddWithValue("@Title", toDo.Title);
+                    comm.Parameters.AddWithValue("@Description", toDo.Description);
+                    comm.Parameters.AddWithValue("@Status", toDo.Status);
+                    comm.Parameters.AddWithValue("@Date", DateTime.Now);
+
+                    await comm.ExecuteNonQueryAsync();
+                }
+            }
+        }
+    }
+}
