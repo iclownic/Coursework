6228228f8e82ab5185ba0549888a366a2ca9d20f
Ludmila Pashkevych <161248085+iclownic@users.noreply.github.com>
2024-06-06 19:09:29 +0300

    Delete WebApplication1 directory

--- WebApplication1/Model/ToDo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ToDoApi.Model
-{
-    public class ToDo
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public bool IsCompleted { get; set; }
-    }
-}
