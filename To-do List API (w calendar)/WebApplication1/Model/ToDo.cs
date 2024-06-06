d6f7f30d1fd545dedfbe03dac7ce57304b9958be
Ludmila Pashkevych <161248085+iclownic@users.noreply.github.com>
2024-06-06 19:39:54 +0300

    Add files via upload

---
+++ To-do List API (w calendar)/WebApplication1/Model/ToDo.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoApi.Model
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ To-do List API (w calendar)/WebApplication1/Program.cs
@@ -0,0 +1,28 @@
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System.IO;
+using System.Threading;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
